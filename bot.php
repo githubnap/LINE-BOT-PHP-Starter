3ab207766f131e71fa0f066729e9f36696ee41e2
githubnap <napsudtay@gmail.com>
2016-11-11 01:35:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,9 +17,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			// $messages = [
+			// 	'type' => 'text',
+			// 	'text' => $text
+			// ];
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'Nap มาเยือน'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
@@ -45,4 +49,4 @@ if (!is_null($events['events'])) {
 		
 	}
 }
-echo "OK";
+echo "OK1";
