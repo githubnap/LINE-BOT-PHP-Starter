f21a02496582e3d55318341d47fb80a272414396
githubnap <napsudtay@gmail.com>
2016-11-11 00:30:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,10 +22,6 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 			
-			$messages = [
-				'type' => 'text',
-				'text': 'Hi'
-			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
